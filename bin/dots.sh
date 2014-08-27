f134b2bfca9a842eb1f16bdcdb91bb42bc29d462
Pertubation <blomblom@openmailbox.org>
2014-08-27 18:22:30 +0200

    Added install template vor dots.sh

--- bin/dots.sh
+++ bin/dots.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-unset DOTFILES
-unset DOTFOLDER
-unset OVERRIDE
-
-declare -A DOTFILES
-DOTFOLDER="$HOME/.dotfiles"
-
-DOTFILES=(
-    ["$HOME/.bashrc"]="$DOTFOLDER/bash/bashrc"
-    ["$HOME/.bash_profile"]="$DOTFOLDER/bash/bash_profile"
-    ["$HOME/.inputrc"]="$DOTFOLDER/bash/inputrc"
-    ["$HOME/.Xresources"]="$DOTFOLDER/Xresources"
-    ["$HOME/.vimperatorrc"]="$DOTFOLDER/vimperator/vimperatorrc"
-    ["$HOME/.vimperator"]="$DOTFOLDER/vimperator/vimperator_folder"
-    ["$HOME/.vimrc"]="$DOTFOLDER/vim/vimrc"
-    ["$HOME/.vim"]="$DOTFOLDER/vim/vim_folder"
-    ["$HOME/.ctags"]="$DOTFOLDER/ctags"
-    ["${XDG_CONFIG_HOME:-$HOME/.config}/herbstluftwm/autostart"]="$DOTFOLDER/herbstluftwm/autostart"
-    ["${XDG_CONFIG_HOME:-$HOME/.config}/herbstluftwm/herbstcommander"]="$DOTFOLDER/herbstluftwm/herbstcommander"
-)
-
 printerr() {
     tput setaf 1
     echo "ERROR:$(tput sgr0) $@"
@@ -36,17 +15,12 @@ overrideQuestion() {
     done
 }
 
-# CASES:
-# DOTFILE in dotfiles folder exist AND symlink of it exist
-#   -> remove symlink and set new symlink
-# DOTFILE in dotfiles folder exist AND symlink don't exist
-#   -> create symlink
-# DOTFILE in dotfiles folder exist AND symlink don't exist but a normal file
-#   -> IF DOTFILE is a file offer diff ELSE offer directory diff
-# DOTFILE in dotfiles folder doesn't exist AND symlink is a normal file/directory
-#   -> offer to copy file/directory in dotfiles folder
-# DOTFILE in dotfiles folder doesn't exist AND symlink exist
-#   -> display error message
+if [[ ! -f "$1" ]]; then
+    printerr "Please add an vaild install file as an argument."
+    exit 1
+fi
+
+. $1
 
 for link in "${!DOTFILES[@]}"; do
     if [[ -e "${DOTFILES["$link"]}" ]] ; then
