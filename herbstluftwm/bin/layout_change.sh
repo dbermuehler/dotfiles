49958e5841d4ce1bc57b6b413f4b610c6950d0a8
Pertubation <blomblom@openmailbox.org>
2014-08-29 14:26:51 +0200

    Restructured of herbstluftwm folder. Added bin folder with herbstluftwm scripts to it. Also $PATH is now set in .bashrc with path to herbstluftwm bin folder.

---
+++ herbstluftwm/bin/layout_change.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hc=herbstclient
+
+case "$(echo -e "grid\nmax\nvertical\nhorizontal" | dmenu -i -p "Layout")" in
+    vertical)   $hc set_layout vertical ;;
+    horizontal) $hc set_layout horizontal ;;
+    max)        $hc set_layout max ;;
+    grid)       $hc set_layout grid ;;
+esac
