abd4d9ffc43aea7fe0905bfc3ca0049db4f0111a
Pertubation <blomblom@openmailbox.org>
2017-04-09 09:00:41 +0200

    Added monitor manager script to herbstluftwm folder

---
+++ herbstluftwm/.config/herbstluftwm/bin/monitor_manager.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+FIRST_MONITOR="LVDS-1"
+SECOND_MONITOR="$(xrandr | sed -nr 's/(VGA-[0-9]).+/\1/p')"
+
+hc() {
+    herbstclient "$@"
+}
+
+up() {
+   xrandr --auto
+   xrandr --output "$SECOND_MONITOR" --above $FIRST_MONITOR
+   hc pad 0 0
+   hc pad 1 25
+   hc emit_hook monitor_changed
+}
+
+down() {
+   xrandr --auto
+   xrandr --output "$SECOND_MONITOR" --below $FIRST_MONITOR
+   hc pad 1 0
+   hc pad 0 25
+   hc emit_hook monitor_changed
+}
+
+right() {
+   xrandr --auto
+   xrandr --output "$SECOND_MONITOR" --right-of $FIRST_MONITOR
+   hc pad 1 0
+   hc pad 0 25
+   hc emit_hook monitor_changed
+}
+
+left() {
+   xrandr --auto
+   xrandr --output "$SECOND_MONITOR" --left-of $FIRST_MONITOR
+   hc pad 0 0
+   hc pad 1 25
+   hc emit_hook monitor_changed
+}
+
+single() {
+   xrandr --auto
+   xrandr --output "$SECOND_MONITOR" --off
+   hc pad 0 25
+   hc emit_hook monitor_changed
+}
+
+auto() {
+   if [[ "disconnect" = "$(cat /sys/class/drm/card1-$SECOND_MONITOR/status)" ]] ; then
+       single
+   else
+       right
+   fi
+}
+
+case "$1" in
+      up) up ;;
+    down) down ;;
+   right) right ;;
+    left) left ;;
+  single) single ;;
+       *) auto;;
+esac
+
+nitrogen --restore
